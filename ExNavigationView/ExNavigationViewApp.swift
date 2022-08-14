c876f63189dd67a83b4085c67f97e2caa4b20274
jake <palatable7@naver.com>
2022-08-14 20:57:23 +0900

    Initial Commit

---
+++ ExNavigationView/ExNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExNavigationViewApp.swift
+//  ExNavigationView
+//
+//  Created by 김종권 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct ExNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
