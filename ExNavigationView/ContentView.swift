7306e92044d98352dc5366a145188611546557bb
jake <palatable7@naver.com>
2022-08-15 13:51:34 +0900

    Implement

--- ExNavigationView/ContentView.swift
+++ ExNavigationView/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    NavigationView {
+      List {
+        NavigationLink(destination: Text("Detail View1")) {
+          Text("Link1")
+        }
+        NavigationLink(destination: Text("Detail View2")) {
+          Text("Link2")
+        }
+        NavigationLink(destination: Text("Detail View3")) {
+          Text("Link3")
+        }
+      }
+      .navigationTitle("Title") // <-
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
